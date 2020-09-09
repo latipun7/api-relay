39903632ddf7225c9d420860a089965e392515ba
LS <latifsulistyo.me@gmail.com>
2020-09-09 16:27:09 +0700

    feat(test): add mongoDB environment 🥭

--- test/jest.setup.ts
+++ test/jest.setup.ts
@@ -1,10 +1,24 @@
 /* eslint-disable import/prefer-default-export */
 import url from 'url';
-import type { Server } from 'http';
-
 import app from 'src/app';
+import type { Server } from 'http';
+import type { Mongoose } from 'mongoose';
 
 const port = app.get('port') || 3541;
+const mongoose: Mongoose = app.get('mongooseClient');
+
+let server: Server;
+
+beforeAll((done) => {
+  server = app.listen(port);
+  server.once('listening', () => done());
+});
+
+afterAll(async (done) => {
+  await mongoose.connection.close();
+  await mongoose.disconnect();
+  server.close(done);
+});
 
 export const getUrl = (pathname?: string, search?: string): string =>
   url.format({
@@ -14,14 +28,3 @@ export const getUrl = (pathname?: string, search?: string): string =>
     pathname,
     search,
   });
-
-let server: Server;
-
-beforeAll((done) => {
-  server = app.listen(port);
-  server.once('listening', () => done());
-});
-
-afterAll((done) => {
-  server.close(done);
-});
